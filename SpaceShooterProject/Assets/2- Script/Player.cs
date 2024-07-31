de032bfdd2ec1d5c7b9c7f8c3d38099f03612a02
Maryam Ansari <145547172+MarioSary@users.noreply.github.com>
2024-07-31 12:56:27 -0800

    Add Clamp

--- SpaceShooterProject/Assets/2- Script/Player.cs
+++ SpaceShooterProject/Assets/2- Script/Player.cs
@@ -21,22 +21,15 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0).normalized;
         transform.Translate(direction * _speed * Time.deltaTime);
 
-        if (transform.position.y >= 7.9f)
-        { 
-            transform.position = new Vector3(transform.position.x, 7.9f, 0);
-        }
-        else if (transform.position.y <= -1.9)
-        {
-            transform.position = new Vector3(transform.position.x, -1.9f, 0);
-        }
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -1.9f, 7.9f), 0);
 
-        if (transform.position.x >= 9.15f)
+        if (transform.position.x >= 11.3f)
         {
-            transform.position = new Vector3(-9.15f, transform.position.y, 0);
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
         }
-        else if (transform.position.x <= -9.15f)
+        else if (transform.position.x <= -11.3f)
         {
-            transform.position = new Vector3(9.15f, transform.position.y, 0);
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
     }
 }
